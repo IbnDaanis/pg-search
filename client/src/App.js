b7bb2bb9447e3fab9accd5a4246f41a0cffda85c
Ikraam <IbnDaanis@Gmail.com>
2021-07-12 20:10:52 +0100

    form value

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 
 export const App = () => {
-  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
 
   const handleSubmit = event => {
     event.preventDefault()
+
+    console.log(query)
   }
 
   return (
@@ -12,7 +14,14 @@ export const App = () => {
       <div className='container text-center'>
         <h1 className='my-5'>User List</h1>
         <form className='d-flex' onSubmit={handleSubmit}>
-          <input type='text' name='name' placeholder='Search user...' className='form-control' />
+          <input
+            type='text'
+            name='name'
+            placeholder='Search user...'
+            className='form-control'
+            value={query}
+            onChange={({ target }) => setQuery(target.value)}
+          />
           <button className='btn btn-success'>Submit</button>
         </form>
       </div>
