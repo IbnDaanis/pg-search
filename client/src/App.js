b55d1a8f545142667d01cd3eed23cb3e29ba1125
Ikraam <IbnDaanis@Gmail.com>
2021-07-12 20:09:36 +0100

    search form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const App = () => {
-  return <div>Hello</div>
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <>
+      <div className='container text-center'>
+        <h1 className='my-5'>User List</h1>
+        <form className='d-flex' onSubmit={handleSubmit}>
+          <input type='text' name='name' placeholder='Search user...' className='form-control' />
+          <button className='btn btn-success'>Submit</button>
+        </form>
+      </div>
+    </>
+  )
 }
