cdf19ad6f80c6db7173f2719cf8dc270e047a470
Ikraam <IbnDaanis@Gmail.com>
2021-07-12 18:53:42 +0100

    users route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const app = express()
 
 app.use(cors())
 
+app.get('/users', async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
